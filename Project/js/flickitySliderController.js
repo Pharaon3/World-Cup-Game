dfea5cb4991946cbedffa8c5fd46c7ab8faeb382
Pharaon3 <Pharaon313@outlook.com>
2022-11-02 21:06:13 -0400

    mobile version grupo flicking implement

--- Project/js/flickitySliderController.js
+++ Project/js/flickitySliderController.js
@@ -1,31 +1,63 @@
 var current_position = 0;
+const flickity_slider = document.getElementsByClassName("flickity-slider");
 function leftArrowButtonClicked() {
-  if (current_position < 0) {
-    current_position += 50;
-    const flickity_slider = document.getElementsByClassName("flickity-slider");
+  var w = window.innerWidth;
+  if(w > 640){
+    if (current_position < 0) {
+      current_position += 50;
+      flickity_slider[0].style.marginLeft = current_position + "px";
+    }
+    else flickity_slider[0].style.marginLeft ="0px";
+  }
+  else {
+    var tmp = current_position;
+    var tmpUnit = 293;
+    current_position = - tmpUnit * 6;
+    if (tmp >= - tmpUnit * 7) current_position = - tmpUnit * 6;
+    if (tmp >= - tmpUnit * 6) current_position = - tmpUnit * 5;
+    if (tmp >= - tmpUnit * 5) current_position = - tmpUnit * 4;
+    if (tmp >= - tmpUnit * 4) current_position = - tmpUnit * 3;
+    if (tmp >= - tmpUnit * 3) current_position = - tmpUnit * 2;
+    if (tmp >= - tmpUnit * 2) current_position = - tmpUnit * 1;
+    if (tmp >= - tmpUnit * 1) current_position = - tmpUnit * 0;
+    if (tmp >= 0) current_position = 0;
     flickity_slider[0].style.marginLeft = current_position + "px";
   }
 }
 function rightArrowButtonClicked() {
   var w = window.innerWidth;
-  var maxLeft = 1050;
-  // 1340
-  // 1060
-  if (w < 1400) {
-    maxLeft = 2244 - w * 0.85;
+  if(w > 640){
+    var maxLeft = 1050;
+    // 1340
+    // 1060
+    if (w < 1400) {
+      maxLeft = 2244 - w * 0.85;
+    }
+    if (w < 1000) maxLeft += 35;
+    if (w < 700) maxLeft += 35;
+    if (w < 550) maxLeft += 15;
+    if (current_position > -maxLeft) {
+      current_position -= 50;
+      flickity_slider[0].style.marginLeft = current_position + "px";
+    } else {
+      current_position - maxLeft;
+      flickity_slider[0].style.marginLeft = current_position + "px";
+    }
   }
-  if (w < 1000) maxLeft += 35;
-  if (w < 700) maxLeft += 35;
-  if (w < 550) maxLeft += 15;
-  if (current_position > -maxLeft) {
-    current_position -= 50;
-    const flickity_slider = document.getElementsByClassName("flickity-slider");
-    flickity_slider[0].style.marginLeft = current_position + "px";
-  } else {
-    current_position - maxLeft;
-    const flickity_slider = document.getElementsByClassName("flickity-slider");
-    flickity_slider[0].style.marginLeft = current_position + "px";
+  else {
+    var tmp = current_position;
+    var tmpUnit = 293;
+    if (tmp > - tmpUnit * 7) current_position = - tmpUnit * 7;
+    if (tmp > - tmpUnit * 6) current_position = - tmpUnit * 6;
+    if (tmp > - tmpUnit * 5) current_position = - tmpUnit * 5;
+    if (tmp > - tmpUnit * 4) current_position = - tmpUnit * 4;
+    if (tmp > - tmpUnit * 3) current_position = - tmpUnit * 3;
+    if (tmp > - tmpUnit * 2) current_position = - tmpUnit * 2;
+    if (tmp > - tmpUnit * 1) current_position = - tmpUnit * 1;
+    if (tmp > 0) current_position = 0;
+    flickity_slider[0].style.marginLeft = current_position + "px";    
   }
+
 }
 window.addEventListener("resize", screenSizeChanged);
 function screenSizeChanged() {
@@ -39,7 +71,6 @@ function screenSizeChanged() {
   if (w < 1000) maxLeft += 35;
   if (w < 700) maxLeft += 35;
   if (w < 550) maxLeft += 15;
-  const flickity_slider = document.getElementsByClassName("flickity-slider");
   var str = flickity_slider[0].style.marginLeft;
   var current_position = str.slice(0, str.length - 2);
   if (current_position < -maxLeft) {
@@ -59,3 +90,4 @@ function screenSizeChanged() {
   document.getElementById("upcham").style.display = "block";
   if (w < 640) document.getElementById("upcham").style.display = "none";
 }
+
